e62ad281cb6210cac1d4d3f0bf9bc7da9f81e99d
laurenlarsen10 <59889370+laurenlarsen10@users.noreply.github.com>
2020-02-04 15:54:26 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/university')
+def university():
+    return render_template('university.html')
+
+@app.route('/hello/<name>')
+def hello(name):
+    return render_template('page.html', name=name)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/cakes')
+def cakes():
+    return render_template('cakes.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port= 1000)
