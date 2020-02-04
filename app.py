91e32270798834ecfecfe9d5449587c85d7b0fff
laurenlarsen10 <59889370+laurenlarsen10@users.noreply.github.com>
2020-02-04 15:47:19 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/hello/<name>')
+def hello(name):
+    return render_template('page.html', name=name)
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/cakes')
 def cakes():
-    return 'Yummy cakes!'
+    return render_template('cakes.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port= 1000)
